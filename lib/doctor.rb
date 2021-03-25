bfcaa6c6c4445024767aaffe40475f40d98b1fc5
Sam D <samidiaz3@gmail.com>
2021-03-25 01:47:16 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select do |appointment|
+    appointment.all.select do |appointment|
       appointment.doctor == self
     end
   end
