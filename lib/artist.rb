b6d99e5ac6ec7cd7d79878e236b725b5d532d238
Sam D <samidiaz3@gmail.com>
2021-03-25 01:11:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,16 @@
 class Artist
 
+  attr_accessor :name
   @@all = []
 
+  def initialize (name)
+    @name = name
+  end
+
+
+  def self.all
+      @@all
+  end
 
 end
 #learn spec/01_artist_spec.rb
