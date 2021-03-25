3ffd11070dc4d372b390d76b7e313f10a4186c16
Sam D <samidiaz3@gmail.com>
2021-03-25 02:06:34 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,33 @@
+class Doctor
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    patients = appointments.collect do |appointment|
+      appointment.patient
+    end
+    patients
+  end
+
+end
+#learn spec/04_doctor_spec.rb
