8b19e482af9f5411ce7c616c8cc59a7317f33e73
Sam D <samidiaz3@gmail.com>
2021-03-25 01:56:49 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,18 +1,18 @@
 class Appointment
 
-  attr_accessor :date, :paitent, :doctor
-  @@all = []
-
-  def initialize(date, paitent, doctor)
-    @date = date
-    @paitent = paitent
-    @doctor = doctor
-    @@all << self
-  end
-
-  def self.all
-    @@all?
-  end
+  # attr_accessor :date, :paitent, :doctor
+  # @@all = []
+  #
+  # def initialize(date, paitent, doctor)
+  #   @date = date
+  #   @paitent = paitent
+  #   @doctor = doctor
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all?
+  # end
 
 
 end
